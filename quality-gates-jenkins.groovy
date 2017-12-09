4d20de85f4690700e4526782aeb0bce1e061710f
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2017-12-09 00:02:14 +0000

    Fix status issue on quality gates script

--- quality-gates-jenkins.groovy
+++ quality-gates-jenkins.groovy
@@ -2,24 +2,28 @@
     @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
 )
 import static groovyx.net.http.ContentType.TEXT
+// Define URL variable
+def taskId = "/var/lib/jenkins/groovy-script/taskId.sh".execute().text.replaceAll("\\n","")
+String taskURL = "http://192.168.1.111:9002/api/ce/task?id="+taskId
+String projectStatusURL = "hhttp://192.168.1.111:9002/api/qualitygates/project_status?analysisId="
 
-def taskClient = new groovyx.net.http.HTTPBuilder('http://<SONAR_URL>/api/ce/task?id=TASK_ID')
-taskClient.setHeaders(Accept: 'application/json')
-
-def taskResponse = taskClient.get(contentType: TEXT)
-def taskSlurper = new groovy.json.JsonSlurper().parse(taskResponse)
-def status=taskSlurper.task.status
-
+// Get project status
+def status=taskURL(taskURL).task.status
 while ( status == "PENDING" || status == "IN_PROGRESS" ) {
 println "waiting for sonar results"
+status=taskURL(taskURL).task.status
 sleep(1000)
 }
       assert status != "CANCELED" : "Build fail because sonar project is CANCELED"
       assert status != "FAILED" : "Build fail because sonar project is FAILED"
-      qualitygatesClient = new groovyx.net.http.HTTPBuilder('http://<SONAR_URL>//api/qualitygates/project_status?analysisId='+taskSlurper.task.analysisId)
-      qualitygatesClient.setHeaders(Accept: 'application/json')
-      qualitygatesResponse = qualitygatesClient.get(contentType: TEXT)
-      def qualitygates= new groovy.json.JsonSlurper().parse(qualitygatesResponse)
+      def qualitygates= httpClient('http://<SONAR_URL>//api/qualitygates/project_status?analysisId='+taskSlurper.task.analysisId)
       assert qualitygates.projectStatus.status != "ERROR" : "Build fail because sonar project status is not ok"
       println "Huraaaah! You made it :) Sonar Results are good"
+
+def httpClient(String url){
+    def taskClient = new groovyx.net.http.HTTPBuilder(url)
+    taskClient.setHeaders(Accept: 'application/json')
+    def response =  taskClient.get(contentType: TEXT)
+    def sluper = def new groovy.json.JsonSlurper().parse(response)
+}      
 
